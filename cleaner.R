ea112ca7c57c2de8cd5077d70c318dfbc466b17e
wesleykg <wesley.gerelle@gmail.com>
2016-02-03 11:59:03 -0800

    work on subsetting

--- cleaner.R
+++ cleaner.R
@@ -1,15 +1,17 @@
-tsStats_file <- "SCEB-Transrate-statistics.tsv"
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(Biostrings))
 
-library(dplyr)
-library(Biostrings)
+transcriptome_file <- "SCEB-SOAPdenovo-Trans-assembly.fa"
+transratestats_file <- "SCEB-Transrate-statistics.tsv"
 
-tsDat <- read.delim(file = tsStats_file, stringsAsFactors = FALSE, 
+Tdat <- readDNAStringSet(filepath = transcriptome_file)
+# all_scaffolds <- names(Tdat)
+
+TRSdat <- read.delim(file = transratestats_file, stringsAsFactors = FALSE, 
                     skip = 44) # First 44 lines are a summary of the data
+names(TRSdat)[1] <- 'name' # Simplify 1st column name
+good_scaffolds <- TRSdat %>% filter(quality == "good") %>% select(name) %>%
+  unlist
 
-names(tsDat)[1] <- 'name' # Simplify 1st column name
-
-good_scaffolds <- tsDat %>% filter(quality == "good") %>% select(name) %>% unlist
-
-SCEBdat <- readDNAStringSet(filepath = "SCEB-SOAPdenovo-Trans-assembly.fa")
-
-
+Cleandat <- subset(Tdat, name = good_scaffolds, select = name)
+identical(Cleandat, Tdat)
