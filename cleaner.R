88a758ed748aa27a173a80537651d0adc074378d
wesleykg <wesley.gerelle@gmail.com>
2016-02-03 15:29:57 -0800

    added comments; still working on subsetting

--- cleaner.R
+++ cleaner.R
@@ -1,17 +1,30 @@
+##Load packages
 suppressPackageStartupMessages(library(dplyr))
 suppressPackageStartupMessages(library(Biostrings))
 
+##List filenames
 transcriptome_file <- "SCEB-SOAPdenovo-Trans-assembly.fa"
 transratestats_file <- "SCEB-Transrate-statistics.tsv"
 
+##Read in a single species transcriptome data
 Tdat <- readDNAStringSet(filepath = transcriptome_file)
-# all_scaffolds <- names(Tdat)
 
+##Read in transrate stats data
 TRSdat <- read.delim(file = transratestats_file, stringsAsFactors = FALSE, 
                     skip = 44) # First 44 lines are a summary of the data
-names(TRSdat)[1] <- 'name' # Simplify 1st column name
-good_scaffolds <- TRSdat %>% filter(quality == "good") %>% select(name) %>%
-  unlist
+names(TRSdat)[1] <- 'names' # Simplify 1st column name
 
-Cleandat <- subset(Tdat, name = good_scaffolds, select = name)
-identical(Cleandat, Tdat)
+##Produce a list of scaffold names that have good quality
+good_scaffolds_names <- TRSdat %>% filter(quality == "good") %>%
+  select(names) %>% unlist
+
+##Create a DNAStringSet object
+good_scaffolds = NULL
+for (i in 1:length(good_scaffolds_names))
+  good_scaffolds = c(good_scaffolds, paste0(""))
+good_scaffolds <- DNAStringSet(good_scaffolds)
+names(good_scaffolds)[1:length(good_scaffolds_names)] <- good_scaffolds_names
+str(good_scaffolds)
+
+##Stuck here
+Cdat <- subset(Tdat, subset = good_scaffolds)
