e83953ac0e41ff9279806add447531f0db72c595
wesleykg <wesley.gerelle@gmail.com>
2016-02-04 11:48:34 -0800

    subsetting works

--- cleaner.R
+++ cleaner.R
@@ -1,30 +1,26 @@
-##Load packages
+## Load packages
 suppressPackageStartupMessages(library(dplyr))
 suppressPackageStartupMessages(library(Biostrings))
 
-##List filenames
+## List filenames
 transcriptome_file <- "SCEB-SOAPdenovo-Trans-assembly.fa"
 transratestats_file <- "SCEB-Transrate-statistics.tsv"
 
-##Read in a single species transcriptome data
+## Read in a single species transcriptome data
 Tdat <- readDNAStringSet(filepath = transcriptome_file)
 
-##Read in transrate stats data
+## ead in transrate stats data
 TRSdat <- read.delim(file = transratestats_file, stringsAsFactors = FALSE, 
                     skip = 44) # First 44 lines are a summary of the data
 names(TRSdat)[1] <- 'names' # Simplify 1st column name
 
-##Produce a list of scaffold names that have good quality
+## Produce a list of names of good quality scaffolds
 good_scaffolds_names <- TRSdat %>% filter(quality == "good") %>%
   select(names) %>% unlist
 
-##Create a DNAStringSet object with just good scaffold names
-good_scaffolds = NULL
-for (i in 1:length(good_scaffolds_names))
-  good_scaffolds = c(good_scaffolds, paste0("ATCG"))
-good_scaffolds <- DNAStringSet(good_scaffolds)
-names(good_scaffolds)[1:length(good_scaffolds_names)] <- good_scaffolds_names
-str(good_scaffolds)
+## Produce a vector that indexes the quality of each scaffold
+scaffolds_index <- names(Tdat) %in% good_scaffolds_names
 
-##Stuck here
-Cdat <- subset(Tdat, subset = good_scaffolds)
+## Removes all bad scaffolds
+Cdat <- Tdat[scaffolds_index]
+
